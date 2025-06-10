98f207cf781b9dd1218159c214cb3750101a52ac
Fatih Karaoğlanoğlu <fkaraogl@cedar2.int.cedar.computecanada.ca>
2025-06-10 10:41:24 -0700

    Build script improvement

--- build.py
+++ build.py
@@ -1,23 +1,52 @@
 import subprocess
 import sys
 import os
+import argparse
 
 from pathlib import Path
 from glob import glob
-from typing import Optional, List, Dict
+from typing import Optional, List, Dict, Union
+from types import FunctionType
 
-USE_VENV=True
-DRY_RUN=False
+
+
+def pip_install (*args):
+    cmd = CMD("pip", "install")
+    for a in args:
+        cmd.append(a)
+    return subprocess.run(cmd)
+
+installers = {
+    "pip": pip_install
+}
 
 class tool:
     name: str
-    install: tuple[str, str, Optional[str]]
+    installation: Union[str, list, FunctionType]
     depends: List["tool"]
-    def __init__ (self, name, install, depends):
+    done: bool
+    def __init__ (self, name, installation, depends):
         self.name = name
-        self.install= install
+        self.installation= installation
         self.depends = depends
+        self.done = False
+    def install(self, tools_dict):
 
+        if self.done:
+            return
+        for tool in self.depends:
+            tools_dict[tool].install(tools_dict)
+        ins = self.installation
+
+        print(f"\n#Installing {self.name}", file=sys.stderr)
+        if not isinstance(ins, list):
+            ins = [ins, self.name]
+        if isinstance(ins[0], str):
+            installers[ins[0]](*ins[1:])
+        else:
+            ins[0](*ins[1:])
+
+        self.done = True
     
 class toolset:
     tools : Dict[str,"tool"]
@@ -25,14 +54,9 @@ class toolset:
     def __init__(self, tools):
         self.tools = {t[0] : tool(t[0], t[1], t[2]) for t in tools}
 
-    def install(self):
-        for name, t in self.tools.items():
-            print(f"Installing {name}", file=sys.stderr)
-            if t.install[0]:
-                cmd = CMD(t.install[0], "install", t.install[1] if t.install[1] else name)
-                ret = subprocess.run(cmd)
-            else:
-                t.install[2]()
+    def install(self, tool_name):
+        assert tool_name in self.tools
+        self.tools[tool_name].install(self.tools)
 
 def CMD(*args):
     cmd = []
@@ -41,7 +65,7 @@ def CMD(*args):
     for a in args:
         cmd.append(a)
     return cmd
-def install_prodigal_function():
+def install_prodigal_function(*x):
     cmd = CMD(
         "git",
         "clone",
@@ -70,7 +94,7 @@ def install_prodigal_function():
     
     os.chdir(cwd)
 
-def install_spades_function():
+def install_spades_function(*x):
     cmd = CMD("wget", "https://github.com/ablab/spades/releases/download/v4.2.0/SPAdes-4.2.0-Linux.tar.gz")
     subprocess.run(cmd)
     cmd = CMD("tar", "-xzf", "SPAdes-4.2.0-Linux.tar.gz")
@@ -81,7 +105,7 @@ def install_spades_function():
     cmd.append(f"{ENV_DIR}")
     subprocess.run(cmd)
 
-def install_hyplass_function():
+def install_hyplass_function(*x):
     cmd = CMD("make")
     subprocess.run(cmd)
     
@@ -93,7 +117,7 @@ def install_hyplass_function():
         )
     subprocess.run(cmd)
 
-def install_minigraph_function():
+def install_minigraph_function(*x):
     cmd = CMD("git", "clone", "https://github.com/lh3/minigraph.git")
     subprocess.run(cmd)
 
@@ -111,7 +135,7 @@ def install_minigraph_function():
 
     os.chdir(cwd)
 
-def install_racon_function():
+def install_racon_function(*x):
     cmd = CMD("git", "clone", "--recursive", "https://github.com/lbcb-sci/racon.git", "racon")
     subprocess.run(cmd)
 
@@ -133,19 +157,32 @@ def install_racon_function():
     os.chdir(cwd)
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Hyplass Build Script")
+
+    parser.add_argument("env", help="Path to virtual environment")
+    parser.add_argument("-n", "--dry-run", help="Dry run", action="store_true")
+    parser.add_argument("--tool", help="Tool to install", default="hyplass")
+
+    args = parser.parse_args()
+
     global ENV_DIR
-    ENV_DIR = str(Path(sys.argv[1]).resolve())
+    global DRY_RUN
+    DRY_RUN = args.dry_run
+    ENV_DIR = args.env
+
+
     t = toolset([
-        ["pandas", ("pip",None, None), []],
-        ["numpy", ("pip",None,None), []],
-        ["packaging", ("pip", None, None), []],
-        ["platon", ("pip","cb-platon",None), ["prodigal"]],
-        ["prodigal", (None, None, install_prodigal_function), []],
-        ["spades", (None, None, install_spades_function), []], 
-        ["racon", (None, None, install_racon_function), []],
-        ["unicycler", ("pip", "git+https://github.com/f0t1h/Unicycler.git", None), ["spades", "racon", "blast+"]],
-        ["hyplass", (None, None, install_hyplass_function), ["unicycler", "platon", "packaging", "numpy", "pandas"]],
-        ["minigraph", (None, None, install_minigraph_function), []]
+        ["hyplass", install_hyplass_function, ["unicycler", "platon", "packaging", "numpy", "pandas", "minigraph"]],
+        ["pandas", "pip", ["numpy"]],
+        ["numpy", "pip", []],
+        ["packaging", "pip", []],
+        ["platon", ["pip","cb-platon"], ["prodigal"]],
+        ["prodigal", install_prodigal_function, []],
+        ["spades", install_spades_function, []], 
+        ["racon", install_racon_function, []],
+        ["unicycler", ["pip", "git+https://github.com/f0t1h/Unicycler.git"], ["spades", "racon"]],
+        ["minigraph", install_minigraph_function, []]
     ])
-    print("Nice")
-    t.install()
+
+    t.install(args.tool)
